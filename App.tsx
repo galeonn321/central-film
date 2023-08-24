078cf9b69331059510401ea7973071426b906ee0
Santiago <santichiste@hotmail.com>
2023-08-24 08:33:57 +0200

    add logger

--- App.tsx
+++ App.tsx
@@ -4,17 +4,27 @@ import {
   Box,
   config,
   Button,
+  Input,
+  InputField,
 } from "@gluestack-ui/themed";
+import { SafeAreaView } from "react-native";
+import { useFonts } from "expo-font";
+import { LOG } from "./src/config/logger";
 
 export default function App() {
+  const queso = () => {
+    LOG.info("queso");
+  };
   return (
     <GluestackUIProvider config={config.theme}>
-      <Box width="100%" justifyContent="center" alignItems="center" flex={1}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Button>
-          <Text color="#fff">queso</Text>
-        </Button>
-      </Box>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Box width="100%" justifyContent="center" alignItems="center">
+          <Text>Open up App.js to start working on your app!</Text>
+          <Button>
+            <Text color="#fff">queso</Text>
+          </Button>
+        </Box>
+      </SafeAreaView>
     </GluestackUIProvider>
   );
 }
