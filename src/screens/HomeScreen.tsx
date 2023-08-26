8db132af8de1d9426f9acfd8b0ae30c50c4bf87d
Santiago <santichiste@hotmail.com>
2023-08-26 14:48:59 +0200

    add new flashlist

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,12 @@
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, ScrollView } from "react-native";
 import React, { useEffect } from "react";
 import { Box, Button, Image, Pressable, Text } from "@gluestack-ui/themed";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { FlashList } from "@shopify/flash-list";
+
 import { LOG } from "../config/logger";
 import { useNavigation } from "@react-navigation/native";
 import { BlurView } from "expo-blur";
+import FlashList from "@shopify/flash-list/dist/FlashList";
 
 const HomeScreen = () => {
   const navigator = useNavigation();
@@ -14,7 +15,7 @@ const HomeScreen = () => {
       title: "Home",
     });
   }, []);
-  const mockData = [
+  const DATA = [
     {
       title: "First Item",
       image:
@@ -30,54 +31,92 @@ const HomeScreen = () => {
       image:
         "https://images.unsplash.com/photo-1527736947477-2790e28f3443?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2256&q=80",
     },
+    {
+      title: "Third Item",
+      image:
+        "https://images.unsplash.com/photo-1527736947477-2790e28f3443?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2256&q=80",
+    },
+    {
+      title: "Third Item",
+      image:
+        "https://images.unsplash.com/photo-1527736947477-2790e28f3443?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2256&q=80",
+    },
+    {
+      title: "Third Item",
+      image:
+        "https://images.unsplash.com/photo-1527736947477-2790e28f3443?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2256&q=80",
+    },
+    {
+      title: "Third Item",
+      image:
+        "https://images.unsplash.com/photo-1527736947477-2790e28f3443?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2256&q=80",
+    },
+    {
+      title: "Third Item",
+      image:
+        "https://images.unsplash.com/photo-1527736947477-2790e28f3443?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2256&q=80",
+    },
   ];
 
   const renderItem = (item, index) => {
     LOG.info(item);
     return (
-      <Box h="$72" bgColor="#000" w="$32">
-        <Text>{item.title}</Text>
-        <Image source={{ uri: item.image }} size="lg" />
+      <Box>
+        <Image
+          size="2xl"
+          borderRadius={32}
+          source={{ uri: item.image }}
+          my="$4"
+          mx="$4"
+        />
+        <Text color="#fff">{item.title}</Text>
       </Box>
     );
   };
 
   return (
     <SafeAreaView style={{ backgroundColor: "#000" }}>
-      <Image
-        size="xs"
-        borderRadius={32}
-        source={{
-          uri: "https://images.unsplash.com/photo-1527736947477-2790e28f3443?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2256&q=80",
-        }}
-        my="$4"
-        mx="$4"
-      />
-      <Box bgColor="$black">
-        <Text color="#fff" textAlign="center" mt={"$8"} fontWeight="$extrabold">
-          What are you looking for?
-        </Text>
-
-        <FlashList
-          renderItem={renderItem}
-          data={mockData}
-          estimatedItemSize={200}
+      <ScrollView>
+        <Image
+          size="xs"
+          borderRadius={32}
+          source={{
+            uri: "https://images.unsplash.com/photo-1527736947477-2790e28f3443?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2256&q=80",
+          }}
+          my="$4"
+          mx="$4"
         />
-        <Pressable
-          onPress={() => LOG.info("queso")}
-          sx={{ ":pressed": { opacity: 0 } }}
-        >
-          <Image
-            size="2xl"
-            borderRadius={32}
-            source={{
-              uri: "https://images.unsplash.com/photo-1527736947477-2790e28f3443?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2256&q=80",
-            }}
-            alignSelf="center"
-            my="$12"
+        <Box bgColor="$black" mb="$12">
+          <Text
+            color="#fff"
+            textAlign="center"
+            mt={"$8"}
+            fontWeight="$extrabold"
+          >
+            What are you looking for?
+          </Text>
+          <FlashList
+            data={DATA}
+            horizontal
+            renderItem={renderItem}
+            estimatedItemSize={200}
           />
-        </Pressable>
-      </Box>
+          <Pressable
+            onPress={() => LOG.info("queso")}
+            sx={{ ":pressed": { opacity: 0 } }}
+          >
+            <Image
+              size="2xl"
+              borderRadius={32}
+              source={{
+                uri: "https://images.unsplash.com/photo-1527736947477-2790e28f3443?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2256&q=80",
+              }}
+              alignSelf="center"
+              my="$12"
+            />
+          </Pressable>
+        </Box>
+      </ScrollView>
     </SafeAreaView>
   );
 };
