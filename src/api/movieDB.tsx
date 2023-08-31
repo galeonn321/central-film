07f71afdb95df82f715f91a45a9f08d237b03f53
Santiago <santichiste@hotmail.com>
2023-08-31 21:51:41 +0200

    fix issue

--- src/api/movieDB.tsx
+++ src/api/movieDB.tsx
@@ -1,16 +1,16 @@
 import axios from "axios";
 import { API_KEY_MOVIES_TMDb } from "@env";
+import { LOG } from "../config/logger";
 
 const APIKEY = process.env.API_KEY_MOVIES_TMDb;
 
 const movieDB = axios.create({
-  baseURL: "https://api.themoviedb.org/3/search/movie",
+  baseURL: "https://api.themoviedb.org/3/movie",
   params: {
     api_key: APIKEY,
-    language: "en-US",
-    page: 1,
-    include_adult: false,
+    language: "en-US"
   },
 });
+
 
 export default movieDB;
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,66 +1,86 @@
-import { SafeAreaView, ScrollView } from "react-native";
+import { ActivityIndicator, SafeAreaView, ScrollView } from "react-native";
 import React, { useEffect, useState } from "react";
-import { Box, Image, Text } from "@gluestack-ui/themed";
+import { Box, Heading, Image, Input, Pressable, Text } from "@gluestack-ui/themed";
 import { LOG } from "../config/logger";
 import { useNavigation } from "@react-navigation/native";
 import FlashList from "@shopify/flash-list/dist/FlashList";
 import { API_KEY_MOVIES_TMDb } from "@env";
 import movieDB from "../api/movieDB";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const apiKey = process.env.API_KEY_MOVIES_TMDb;
 
 const HomeScreen = () => {
+  const insets = useSafeAreaInsets();
   const navigator = useNavigation();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [films, setFilms] = useState<Object>({});
   useEffect(() => {
     navigator.setOptions({
       title: "Home",
     });
   }, []);
-  useEffect(() => {
-    movieDB({ params: { api_key: API_KEY_MOVIES_TMDb, query: "titanic" } })
-      .then((response) => {
-        // LOG.info(response);
-        setFilms(response.data.results);
-      })
-      .catch((error) => {
-        LOG.error(error);
-      })
-      .finally(() => {
-        LOG.info("finally");
-      });
 
-    LOG.info(movieDB);
+
+  useEffect(() => {
+      movieDB.get('/now_playing').then(
+        (response)=>{
+          LOG.warn(response.data.results, 'HAHAHAHAH');
+          setFilms(response.data.results);
+
+          if(response.data.results?.length > 0){
+
+            setIsLoading(false);
+          }else{
+            setIsLoading(true);
+          }
+        }
+      ).catch(
+        (error)=>{
+          LOG.error(error, ' HAHAHA');
+        }
+      ).finally(
+        ()=>{
+          LOG.info('finally');
+        }
+
+      );
+
+    // LOG.info(movieDB);
   }, []);
 
-  useEffect(() => {
-    // LOG.warn(films);
-  }, [films]);
+  // useEffect(() => {
+  //   LOG.warn(films);
+  // }, [films]);
 
   const renderItem = (item, index) => {
-    LOG.info(item.item.poster_path, item.item.title, item.item.overview);
-    return (
-      <Box>
+    const uri = `https://image.tmdb.org/t/p/w500${item.item?.poster_path}`
+   return (
+      <Pressable 
+      onPress={() => {
+        // navigator.navigate("Details", { id: item.item?.id });
+        LOG.info(item.item?.id)
+      }}
+      
+      >
         <Image
-          size="2xl"
+          size={'2xl'}
           borderRadius={32}
-          source={{ uri: item.item.path }}
-          my="$4"
+          source={{ uri: uri ?? '' }}
+          resizeMode="cover"
           mx="$4"
+          sx={{ ":pressed" : { backgroundColor: "#fff" } }}
         />
-        <Text color="#fff" mx="$4">
+        <Text color="#fff" mx="$4" mt='$4' italic fontWeight="700" fontSize={'$xl'} py={'$2'} maxWidth={'$72'}>
           {item.item?.title}
         </Text>
-        <Text color="#fff" mx="$4" maxWidth={"$32"} flexWrap="wrap">
-          {item.item?.overview}
-        </Text>
-      </Box>
+      </Pressable>
     );
   };
 
   return (
-    <SafeAreaView style={{ backgroundColor: "#000" }}>
-      <ScrollView>
+    <SafeAreaView style={{ backgroundColor: "#000" }}  >
+      <ScrollView >
         <Image
           size="xs"
           borderRadius={32}
@@ -70,24 +90,47 @@ const HomeScreen = () => {
           my="$4"
           mx="$4"
         />
-        <Box bgColor="$black" mb="$72">
-          <Text
+        <Box bgColor="$black">
+          <Heading 
             color="#fff"
             textAlign="center"
-            mt={"$8"}
             p={"$2"}
             fontWeight="$extrabold"
             fontSize={"$3xl"}
           >
-            What are you looking for?
-          </Text>
-          <FlashList
-            data={films}
-            horizontal
-            renderItem={renderItem}
-            estimatedItemSize={200}
+            Search and see how good the film is
+          </Heading>
+          <Input
+            placeholder="Search"
+            placeholderTextColor="#fff"
+            bgColor="$black"
+            color="#fff"
+            borderColor="#fff"
+            borderWidth={1}
+            borderRadius={12}
+            mx="$4"
+            my="$4"
+            p="$4"
           />
-
+          <Heading
+            color="#fff"
+            textAlign="center"
+            p={"$2"}
+            fontWeight="$extrabold"
+            fontSize={"$3xl"}
+          >
+            Playing now
+          </Heading>
+          {isLoading ? <ActivityIndicator size="large" color="#fff" /> :
+          <Box>
+            <FlashList
+              data={films}
+              horizontal
+              renderItem={renderItem}
+              estimatedItemSize={200}
+            />
+          </Box>
+          }
           <Image
             size="2xl"
             borderRadius={32}
@@ -96,6 +139,7 @@ const HomeScreen = () => {
             }}
             alignSelf="center"
             my="$12"
+            resizeMode="cover"
           />
         </Box>
       </ScrollView>
