99db6b05e1204bcb181707564c2a501200c44c32
galeonn321 <santichiste@hotmail.com>
2023-10-20 11:37:20 +0200

    add new changes gluestack

--- App.tsx
+++ App.tsx
@@ -1,10 +1,7 @@
 import { GluestackUIProvider, config } from "@gluestack-ui/themed";
-
-import { useFonts } from "expo-font";
 import BottomTabNavigator from "./src/navigators/BottomTabNavigator";
 
 import { NavigationContainer } from "@react-navigation/native";
-import { SafeAreaView } from "react-native";
 import MyStack from "./src/navigators/MainNavigator";
 
 export default function App() {
