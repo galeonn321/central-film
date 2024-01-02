59e9d395a43969e9a9b6560017f061674d08eba3
galeonn321 <santichiste@hotmail.com>
2024-01-03 00:06:00 +0100

    new welcome screen

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -23,34 +23,35 @@ const WelcomeScreen = () => {
   }, []);
 
   return (
-    <Box w="$full" flex={1} bgColor="#fff">
+    <Box w="$full" flex={1} bgColor="#000">
       <Swiper
-        style={{ height: Height / 1.6, backgroundColor: "#fff" }}
-        autoplay={false}
+        style={{ height: Height / 1.6, backgroundColor: "#000" }}
+        autoplay={true}
         showsPagination={true}
         activeDotColor="#7f1d1d"
         dotColor="#666"
-        paginationStyle={{ bottom: Height * 0.1, position: "absolute" }}
+        paginationStyle={{ bottom: Height * 0.12, position: "absolute" }}
         showsButtons={false}
       >
         <Box>
           {/* <Text>Hello Swiper</Text> */}
           <Image
-            source={require("../../assets/images/icon.png")}
-            resizeMode="contain"
+            source={require("../../assets/images/camera-film.jpg")}
+            resizeMode="cover"
             w={Width}
-            h={Height * 0.4}
+            h={Height * 0.5}
             alt="welcome image"
             role="presentation"
+            bgColor="#fff"
           />
           <Box justifyContent="center" alignItems="center" mx="$12">
-            <Text fontSize={"$md"} color="#000">
+            <Text fontSize={"$lg"} color="#fff" mt={"$4"}>
               Welcome to
             </Text>
-            <Heading fontSize={"$3xl"} bold color="#000">
+            <Heading fontSize={"$6xl"} bold color="#fff" pt={"$8"}>
               Central Film
             </Heading>
-            <Text textAlign="center" color="#777">
+            <Text textAlign="center" color="#777" mt={"$2"} fontSize={"$xl"}>
               Discover and share a constantly Expanding mix of movies.
             </Text>
           </Box>
@@ -58,58 +59,56 @@ const WelcomeScreen = () => {
         <Box justifyContent="center" alignItems="center">
           {/* <Text>Hello Swiper</Text> */}
           <Image
-            source={require("../../assets/images/icon.png")}
-            resizeMode="contain"
+            source={require("../../assets/images/controller-tv.jpg")}
+            resizeMode="cover"
             w={Width}
-            h={Height * 0.4}
+            h={Height * 0.5}
             alt="welcome image"
             role="presentation"
           />
-          <Text fontSize={"$md"} color="#fff">
-            Welcome to
+          <Text fontSize={"$lg"} color="#fff" mt={"$2"}>
+            Real
           </Text>
-          <Heading fontSize={"$3xl"} bold color="#fff">
-            Central Film
+          <Heading fontSize={"$6xl"} bold color="#fff" pt={"$8"}>
+            Reviews
           </Heading>
+          <Text textAlign="center" color="#777" mt={"$1"} fontSize={"$xl"}>
+            find out what people are saying about your favorite movies and write
+            your honest opinion without filters.
+          </Text>
         </Box>
         <Box justifyContent="center" alignItems="center">
           {/* <Text>Hello Swiper</Text> */}
           <Image
-            source={require("../../assets/images/icon.png")}
-            resizeMode="contain"
+            source={require("../../assets/images/entrance-cinema.jpg")}
+            resizeMode="cover"
             w={Width}
-            h={Height * 0.4}
+            h={Height * 0.5}
             alt="welcome image"
             role="presentation"
           />
-          <Text fontSize={"$md"} color="#fff">
-            Welcome to
+          <Text fontSize={"$lg"} color="#fff" mt={"$2"}>
+            Quick search for
           </Text>
-          <Heading fontSize={"$3xl"} bold color="#fff">
-            Central Film
+          <Heading fontSize={"$6xl"} bold color="#fff" pt={"$8"}>
+            Films & Series
           </Heading>
+          <Text textAlign="center" color="#777" mt={"$1"} fontSize={"$xl"}>
+            Don't know what to watch? Search for your favorite films and series,
+            read the reviews and make your choice.
+          </Text>
         </Box>
         {/* Remaining slides */}
       </Swiper>
-      {/* <LottieView
-        autoPlay
-        loop={false}
-        style={{
-          width: 200,
-          height: 200,
-          backgroundColor: "#fff",
-        }}
-        source={require("../../assets/lottie/cinema.json")}
-      /> */}
       <Button
         bgColor="$red900"
         mx="$10"
         rounded={"$full"}
-        bottom={Height * 0.07}
+        bottom={Height * 0.08}
       >
-        <ButtonText>Login</ButtonText>
+        <ButtonText bold>Login</ButtonText>
       </Button>
-      <Text textAlign="center" bottom={Height * 0.07}>
+      <Text textAlign="center" bold bottom={Height * 0.07}>
         Continue as guest
       </Text>
     </Box>
